4711e1b45dbcf52c87c2eda1b39ed923350aac9f
MdTanwer <tanw9004167@gmail.com>
2025-09-04 06:13:15 +0530

    .

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -5,18 +5,12 @@ console.log(process.env.GOOGLE_CLIENT_ID, "red");
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId:
-        process.env.GOOGLE_CLIENT_ID ||
-        "952911097209-6ksdn7cabtb7sd7sv1qeecpkgob501jp.apps.googleusercontent.com",
-      clientSecret:
-        process.env.GOOGLE_CLIENT_SECRET ||
-        "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",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "45e0032e22edf4cf5a05",
-      clientSecret:
-        process.env.GITHUB_CLIENT_SECRET ||
-        "673da9aa66d1b845bf50f1ea7c2c551fe21f6fcd",
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }),
   ],
   secret: process.env.SECRET || "1234567890",
